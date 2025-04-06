ebae4bdbad370df221c6dea4e348d318a15d3689
Haroon Jawad <haroonjawad6@gmail.com>
2025-04-06 21:26:35 +0500

    Cleanup

--- src/app/UserDashboard/sideBar.tsx
+++ src/app/UserDashboard/sideBar.tsx
@@ -1,50 +1,79 @@
 import React, { useState } from "react";
-import { XMarkIcon, ChartBarIcon, CogIcon, CameraIcon, HeartIcon } from "@heroicons/react/24/outline";
-import { Move } from "lucide-react";
+import {
+  XMarkIcon,
+  ChartBarIcon,
+  CogIcon,
+  CameraIcon,
+  HeartIcon,
+} from "@heroicons/react/24/outline";
+import { Move, Dumbbell } from "lucide-react";
 interface UserDashboardMenuProps {
   setSelectedPage: (page: string) => void;
 }
 
-const UserDashboardMenu: React.FC<UserDashboardMenuProps> = ({ setSelectedPage }) => {
+const UserDashboardMenu: React.FC<UserDashboardMenuProps> = ({
+  setSelectedPage,
+}) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  const DumbbellIcon = () => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="h-5 w-5 mr-3"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M3 10h3v4H3v-4zm15 0h3v4h-3v-4zM7 14h10M7 10h10m-7 4v-4m4 4v-4"
-      />
-    </svg>
-  );
-  
   const sideBarItems = [
-    { name: "Home", icon: <ChartBarIcon className="h-5 w-5 mr-3" />, page: "Analytics" },
-    { name: "Settings", icon: <CogIcon className="h-5 w-5 mr-3" />, page: "Settings" },
+    {
+      name: "Home",
+      icon: <ChartBarIcon className="h-5 w-5 mr-3" />,
+      page: "Analytics",
+    },
+
     // { name: "Goals", icon: <HeartIcon className="h-5 w-5 mr-3" />, page: "Goals" },
-    { name: "Start Workout", icon: DumbbellIcon(), page: "StartWorkout" },
-    { name: "Track Food", icon: <CameraIcon className="h-5 w-5 mr-3" />, page: "UploadFood" },
-    { name: "Personalized Diet", icon: <CameraIcon className="h-5 w-5 mr-3" />, page: "diet-page" },
-    { name: "Personalized Workout Plan", icon: <CameraIcon className="h-5 w-5 mr-3" />, page: "workoutplan-page" },
-    
+    {
+      name: "Start Workout",
+      icon: <Dumbbell className="mr-3" width={"20px"} />,
+
+      page: "StartWorkout",
+    },
+    {
+      name: "Track Food",
+      icon: <CameraIcon className="h-5 w-5 mr-3" />,
+      page: "UploadFood",
+    },
+    {
+      name: "Personalized Diet",
+      icon: <CameraIcon className="h-5 w-5 mr-3" />,
+      page: "diet-page",
+    },
+    {
+      name: "Personalized Workout Plan",
+      icon: <CameraIcon className="h-5 w-5 mr-3" />,
+      page: "workoutplan-page",
+    },
+    {
+      name: "Settings",
+      icon: <CogIcon className="h-5 w-5 mr-3" />,
+      page: "Settings",
+    },
   ];
 
   return (
     <>
       {/* Mobile Sidebar */}
-      <div className={`${sidebarOpen ? "block" : "hidden"} fixed inset-0 z-40 lg:hidden`}>
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-75" onClick={() => setSidebarOpen(false)}></div>
+      <div
+        className={`${
+          sidebarOpen ? "block" : "hidden"
+        } fixed inset-0 z-40 lg:hidden`}
+      >
+        <div
+          className="fixed inset-0 bg-gray-800 bg-opacity-75"
+          onClick={() => setSidebarOpen(false)}
+        ></div>
         <div className="fixed inset-y-0 left-0 flex flex-col w-64 max-w-xs bg-gray-900 border-r border-gray-700 text-white">
           <div className="flex items-center justify-between h-16 px-4 border-b border-gray-700">
-            <span className="text-xl font-semibold"><Move className="h-8 w-8 text-purple-300" />FitMaster AI</span>
-            <button className="text-gray-400 hover:text-white" onClick={() => setSidebarOpen(false)}>
+            <span className="text-xl font-semibold">
+              <Move className="h-8 w-8 text-purple-300" />
+              FitMaster AI
+            </span>
+            <button
+              className="text-gray-400 hover:text-white"
+              onClick={() => setSidebarOpen(false)}
+            >
               <XMarkIcon className="h-6 w-6" />
             </button>
           </div>
